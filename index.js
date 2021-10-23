232aeb6e9ae2ce0c3688a6a78d019d7b9444b54f
Maksim Filanovich <filanovich.maxim11b@mail.ru>
2021-10-23 20:18:39 +0300

    make changes in develop

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const age = 20;
+console.log('age', age);
 if (age > 18) {
-  console.log('Вы совершенолетний');
+  console.info(
+    'Вы совершенолетний',
+  );
+}
+if (age < 18) {
+  console.info('Вы НЕ совершенолетний');
 }
